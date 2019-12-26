9f1cdb3241bf878997ebd2e0c4345a13f65a9e13
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-12-26 10:39:37 +0100

    Exercice

--- HelloAngular/src/app/app.module.ts
+++ HelloAngular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { CounterComponent } from './counter/counter.component';
 import { SelectComponent } from './select/select.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     CounterComponent,
-    SelectComponent
+    SelectComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule
---
+++ HelloAngular/src/app/todolist/todolist.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+
+  todos = ['A', 'B', 'C'];
+  todo = '';
+  /*
+  Exercice 1 :
+  Remplacer les paragraphes <p>Acheter du pain</p>...
+  par le contenu du tableau todos en utilisant *ngFor (comme dans le composant select)
+  
+  Exercice 2 :
+  Ecouter l'événement submit du formulaire avec (submit)
+  Appeler la fonction addTodo depuis le template avec le paramètre $event
+
+  Exercice 3 :
+  Comme dans le composant hello
+  Mettre à jour la propriété todo en fonction du champs présent dans le form
+  Compléter la fonction addTodo pour qu'elle ajouter la valeur saisie dans le tableau
+  todos
+  */
+
+  addTodo(event) {
+    event.preventDefault();
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
